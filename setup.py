4a295ac56db4e325f77d2d21bed2458b7381f411
Magniquick <67406856+Magniquick@users.noreply.github.com>
2025-06-25 23:06:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='gym_uno',
       version='0.0.1',
       install_requires=['gym>=0.2.3'],
-      data_files=[('envs',['gym_uno.envs/uno.jar'])],
+      data_files=[('envs',['gym_uno/envs/uno.jar'])],
       include_package_data=True
 )
